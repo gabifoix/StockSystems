2d7eaa2e557876889a53e2d51fd25d0dab73cd9d
gabrielfoix <gabriel.foix@mirai-solutions.com>
2020-09-12 23:28:28 +0200

    Renamed PortfolioReport html output

---
+++ R/render_Rmd.R
@@ -0,0 +1,4 @@
+
+rmarkdown::render('PortfolioAssessment.Rmd', 
+                  output_dir = "PortfolioReports",
+                  output_file = paste0('PortfolioReport_', gsub("-", "", Sys.Date()), '.html'))
