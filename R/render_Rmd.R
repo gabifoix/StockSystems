c431c169b71e5952a6a04538c2697dac9b45716c
gabrielfoix <gabriel.foix@mirai-solutions.com>
2020-09-15 16:41:11 +0200

    Tickers as param in StockResearch

--- R/render_Rmd.R
+++ R/render_Rmd.R
@@ -2,3 +2,9 @@
 rmarkdown::render('PortfolioAssessment.Rmd', 
                   output_dir = "PortfolioReports",
                   output_file = paste0('PortfolioReport_', gsub("-", "", Sys.Date()), '.html'))
+
+Tickers <- c("UNA.AS", "DGV.MI", "VOW3.DE","PUM.DE",  "MC.PA", "CS.PA", "TEF.MC", "SAN.MC", "AAPL", "FP.PA")
+rmarkdown::render('StockReseach.Rmd', 
+                  output_dir = "StockResearch",
+                  params = list(Tickers = Tickers),
+                  output_file = paste0('StockResearch', gsub("-", "", Sys.Date()), '.html'))
