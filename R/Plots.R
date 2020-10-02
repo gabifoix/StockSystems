4372612033a438d9f989685d892566bfd8430bbe
gabrielfoix <gabriel.foix@mirai-solutions.com>
2020-10-02 22:24:21 +0200

    Several updates

--- R/Plots.R
+++ R/Plots.R
@@ -17,16 +17,36 @@ piechartplot <- function(Alloc.df, date = today()) {
   
 }
 
-# test.df <- data.frame(Ticker = c("UNA.AS" ,"VOW3.DE", "MC.PA", "REE.MC", "PFtest"),
-#                     test = c(0.025156, 0.1556, 0.26569, 0.002021, 0.41235),
+# test.df <- data.frame(Ticker = c("UNA.AS" ,"VOW3.DE", "MC.PA", "REE.MC", "AAA.MC", "BBB", "CCC", "DDDD", "HEHEH", "PFtest"),
+#                     test = c(0.025156, 0.1556, 0.26569, 0.002021, 0.41235, 0.7, 0.15, 0, 0.333, 0.288989),
 #                     stringsAsFactors = FALSE)
-# barchartPFplot(test.df, "test", PFTicker = "PFtest", labelPer = TRUE)
-barchartPFplot <- function(df, VarName, Key = "Ticker", PFTicker = "PF", nround = 2, labelPer = FALSE) {
+# barchartPFplot(test.df, "test", top = 5, PFequallyweigthed = TRUE, labelPer = TRUE)
+barchartPFplot <- function(df, VarName, theHighertheBetter = TRUE, Key = "Ticker", PFTicker = "PF", PFequallyweigthed = FALSE, top = 12, nround = 2, rmZeros = TRUE, labelPer = FALSE) {
  
   # Prepare df
   df <- df[, c(Key, VarName)] %>%  
-    setNames(c("Key", "VarName")) %>% 
-    dplyr::arrange(desc(VarName)) 
+    setNames(c("Key", "VarName"))
+  
+  if (theHighertheBetter) {
+    df <- dplyr::arrange(df, desc(VarName))
+  } else {
+    df <- dplyr::arrange(df, VarName)
+  }
+  
+  if (rmZeros == TRUE) {
+    df <- subset(df, VarName != 0)
+  }
+  
+  if (PFequallyweigthed) {
+    # If PF equally weigthed, the name of the PF can not be decided  
+    stopifnot(PFTicker == "PF")
+    df <- rbind(df, data.frame(Key = PFTicker, VarName = mean(df$VarName)))
+  }
+  
+  # Select top & PF
+  df <- rbind(head(df, top),
+        subset(df, Key == PFTicker)) %>% 
+    unique()
   
   # Prepare labels
   if (labelPer) {
@@ -43,7 +63,7 @@ barchartPFplot <- function(df, VarName, Key = "Ticker", PFTicker = "PF", nround 
                            fill= factor(ifelse(Key == PFTicker, "Highlighted", "Normal")))) + 
     geom_bar(stat = "identity") +
     geom_text(aes(label = labelTop, vjust = -0.5)) +
-    scale_fill_manual(name = "Key", values = c("red","grey50")) +
+    scale_fill_manual(name = "Key", values = c("red", "grey50")) +
     theme(legend.position = "none",
           axis.title.x = element_blank(),
           axis.title.y = element_blank(),
--- R/renderRmd.R
+++ R/renderRmd.R
@@ -13,13 +13,30 @@ rmarkdown::render('PortfolioAssessment.Rmd',
 Tickers <- read.csv("CompanyList.20200713.csv", sep = ";") %>% 
   subset(country == "HO", select = "tickers")
 
-
-SearchName <- "HO"
-Index <- "^FCHI"
+# DJ ----
+SearchName <- "DJ"
+Index <- "^DJI"
 
 Tickers<- YHFinR::getYFIndexComp(Index)$Symbol
 
 rmarkdown::render('StockResearch.Rmd', 
                   output_dir = "StockResearch",
-                  params = list(Ticker = unique(Tickers$tickers)),
+                  params = list(Ticker = unique(Tickers),
+                                Index = "^DJI"),
                   output_file = paste0('StockResearch_', SearchName, "_", gsub("-", "", Sys.Date()), '.html'))
+
+# Europe with industry informed ----
+Tickers <- read.csv("CompanyList.20200713.csv", sep = ";") %>% 
+  subset(!is.na(industry), select = "tickers")
+
+SearchName <- "AllEUR"
+rmarkdown::render('StockResearch.Rmd', 
+                  output_dir = "StockResearch",
+                  params = list(Ticker = unique(Tickers$tickers),
+                                Index = "^DJI"),
+                  output_file = paste0('StockResearch_', SearchName, "_", gsub("-", "", Sys.Date()), '.html'))
+
+
+# SP500 ----
+Tickers<- YHFinR::getYFIndexComp("SP500")$Symbol
+
