13bd19ae21ef530c0c4b45b8f16c6dc16cef43fa
Gabriel <gabriel.foix@mirai-solutions.com>
2021-03-08 23:05:28 +0100

    Generating All US

--- R/renderRmd.R
+++ R/renderRmd.R
@@ -7,7 +7,7 @@ dateout <- gsub("-", "", Sys.Date())
 rmarkdown::render('PortfolioAssessment.Rmd', 
                   output_dir = "PortfolioReports",
                   output_file = paste0('PortfolioReport_', dateout, '.html'),
-                  params = list(dump_file = paste0('PortfolioReport_', dateout, '.csv')))
+                  params = list(dump_file = paste0('PortfolioReport/PortfolioReport_', dateout, '.csv')))
 
 
 # DJ ----
@@ -22,9 +22,9 @@ rmarkdown::render('StockResearch.Rmd',
                                 Index = "^DJI"),
                   output_file = paste0('StockResearch_', SearchName, "_", gsub("-", "", Sys.Date()), '.html'))
 
-# Europe with industry informed ----
+# Europe ----
 Tickers <- read.csv("CompaniesEUR.20210103.csv", sep = ";") %>% 
-  subset(country != " ", select = "tickers")
+  subset(country != "  ", select = "tickers")
 
 SearchName <- "AllEUR"
 rmarkdown::render('StockResearch.Rmd', 
@@ -56,3 +56,30 @@ rmarkdown::render('StockResearch.Rmd',
                   params = list(Ticker = unique(Tickers$Symbol),
                                 Index = "^GSPC"),
                   output_file = paste0('StockResearch_', SearchName, "_", gsub("-", "", Sys.Date()), '.html'))
+
+# NYSE ----
+Tickers <- read.csv("MD/NYSE.20201010.csv", sep = ",") 
+nrow(Tickers)
+
+SearchName <- "NYSE"
+rmarkdown::render('StockResearch.Rmd', 
+                  output_dir = "StockResearch",
+                  params = list(Ticker = unique(Tickers$Symbol),
+                                Index = "^GSPC",
+                                dump_file = paste0('StockResearch/StockResearch_', SearchName, "_", gsub("-", "", Sys.Date()), '.csv')),
+                  output_file = paste0('StockResearch_', SearchName, "_", gsub("-", "", Sys.Date()), '.html'))
+
+# NYSE + NASDAQ + AMEX ----
+Tickers <- rbind( read.csv("MD/NYSE.20201010.csv", sep = ","),
+                  read.csv("MD/NASDAQ.20201010.csv", sep = ","), 
+                  read.delim("MD/AMEX.txt", header = TRUE, sep = "\t"))
+
+SearchName <- "All_US"
+rmarkdown::render('StockResearch.Rmd', 
+                  output_dir = "StockResearch",
+                  params = list(Ticker = unique(Tickers$Symbol),
+                                Index = "^GSPC",
+                                dump_file = paste0('StockResearch/StockResearch_', SearchName, "_", gsub("-", "", Sys.Date()), '.csv')),
+                  output_file = paste0('StockResearch_', SearchName, "_", gsub("-", "", Sys.Date()), '.html'))
+
+
