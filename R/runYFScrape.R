9c865d375761c6a478a3ebac8f5315fd689c0766
Gabriel <gabriel.foix@mirai-solutions.com>
2021-03-25 22:19:06 +0100

    Add runYFScrape

---
+++ R/runYFScrape.R
@@ -0,0 +1,45 @@
+
+library(YHFinR)
+library(dplyr)
+
+# US ----
+Tickers <- read.csv("MD/CompaniesUS.20210319.csv", sep = ";") %>% 
+  subset(Sector != "NAYF" & Sector != "" ) %>% 
+  .[, c("Ticker", "CompanyName", "Country", "Sector", "Industry")] %>% 
+  unique()
+AppendName <- "US"
+scrapeYF(Tickers, AppendName)
+
+# EUR ----
+Tickers <- read.csv("MD/CompaniesEUR.20210103.csv", sep = ";", stringsAsFactors = FALSE) %>% 
+  subset(country == "IT", select = c("CompanyName", "tickers"))
+Tickers <- Tickers[!duplicated(Tickers, fromLast = TRUE),]
+colnames(Tickers) <- c("CompanyName", "Ticker")
+
+AppendName <- "IT"
+scrapeYF(Tickers, AppendName, Hist = FALSE)
+
+
+scrapeYF <- function(Tickers, AppendName, Hist = TRUE) {
+  BS <- getYH_qBS(Tickers$Ticker, block = 100, slp = 10)
+  output_file = paste0('RawData/BS.list_n', length(BS), "_",AppendName, "_", gsub("-", "", Sys.Date()), '.rds')
+  saveRDS(BS, output_file)
+  
+  IS <- getYH_qIS(Tickers$Ticker, block = 100, slp = 10)
+  output_file = paste0('RawData/IS.list_n', length(IS), "_",AppendName, "_", gsub("-", "", Sys.Date()), '.rds')
+  saveRDS(IS, output_file)
+  
+  CF <- getYH_qCF(Tickers$Ticker, block = 100, slp = 10)
+  output_file = paste0('RawData/CF.list_n', length(CF), "_",AppendName, "_", gsub("-", "", Sys.Date()), '.rds')
+  saveRDS(CF, output_file)
+  
+  if (Hist) {
+    HistPrices <- YHFinR::getYFHistPrices(Tickers$Ticker, "5y", "1d", block = 100, slp = 10)
+    output_file = paste0('RawData/HistPr.list_n', length(HistPrices), "_",AppendName, "_", gsub("-", "", Sys.Date()), '.rds')
+    saveRDS(HistPrices, output_file)
+  }
+}
+
+
+
+
